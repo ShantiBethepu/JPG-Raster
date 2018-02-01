af312dcdc3af16b2331c1fa1d3a21a879116336e
che <che@siue.edu>
2018-02-01 15:32:12 -0600

    Add JPGScannerTest skeleton

---
+++ src/test/java/com/ociweb/jpgRaster/JPGScannerTest.java
@@ -0,0 +1,56 @@
+package com.ociweb.jpgRaster;
+
+import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+import org.junit.Test;
+
+public class JPGScannerTest {
+    @Test
+    public void queueFileTest() {
+
+    }
+
+    @Test
+    public void ReadQuantizationTableTest() {
+
+    }
+
+    @Test
+    public void ReadStartOfFrameTest() {
+
+    }
+
+    @Test
+    public void ReadHuffmanTableTest() {
+
+    }
+
+    @Test
+    public void ReadStartOfScanTest() {
+
+    }
+
+    @Test
+    public void ReadRestartIntervalTest() {
+
+    }
+
+    @Test
+    public void ReadRSTNTest() {
+
+    }
+
+    @Test
+    public void ReadAPPNTest() {
+
+    }
+
+    @Test
+    public void ReadCommentTest() {
+        
+    }
+
+    @Test void ReadJPEGTest() {
+
+    }
+}
